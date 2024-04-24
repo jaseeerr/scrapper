df3518fef58cd8e4ba534d00368bd89ae11c74f6
jaseeerr <jaseerhasbulla01@gmail.com>
2024-04-24 14:16:55 +0400

    musicStream1

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,6 +1,6 @@
-export const SERVER_URL = "http://localhost:3000"
+export const SERVER_URL = "http://localhost:5000"
 
-export const SOCKET_URL = "http://localhost:3000"
+export const SOCKET_URL = "http://localhost:5000"
 
 
 // export const SERVER_URL = "https://api.honordubai.com"

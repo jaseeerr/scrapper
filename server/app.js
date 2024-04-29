955dc46c5d49577dabbdf7e93c735952d8710ae8
jaseeerr <jaseerhasbulla01@gmail.com>
2024-04-29 12:16:57 +0400

    musicStream11211222

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+
+const cors = require('cors')
 
 var app = express();
 
@@ -26,6 +29,7 @@ app.use(
     credentials: true,  
   })
 ); 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 

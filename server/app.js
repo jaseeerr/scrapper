efdac370d08467d17c21c7541720da984751558b
jaseeerr <jaseerhasbulla01@gmail.com>
2024-04-29 12:07:04 +0400

    musicStream112112

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://chitchat.website',
     methods: ['GET', 'POST', 'PUT', 'PATCH'],
     credentials: true,  
   })
